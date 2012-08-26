12958ff3bbe7f017a2bb62b2e6db4c5e4400a88b
s.okada <kokuzen@gmail.com>
2012-08-27 02:39:41 +0900

    Node was added. Node enable users to register some on_receive_func.

---
+++ Node.cpp
@@ -0,0 +1,44 @@
+#ifdef NODE_UNIT_TEST
+#include "Node.h"
+#include <iostream>
+
+using namespace nr;
+
+int main(int argc, char* argv[])
+{
+	boost::asio::io_service service;
+	int server_port = 54321;
+	if(argc == 2)
+	{
+		server_port = boost::lexical_cast<int>(argv[1]);
+	}
+
+	auto node = Node<int>(service, server_port, 128, 
+		[](const utl::ByteArray&)->int{return 10;}, std::cout);
+	
+	node.RegisterForUpperFunc(10,
+		[](P2pCore::Pointer core, Session::Pointer session, 
+				const utl::ByteArray& byte_array){ //on receive func from upper
+			std::string str(byte_array.begin(), byte_array.end());
+			std::cout << "onreceive FROM UPPER called:" << str << std::endl;
+			std::cout << core->GetSessionListStr() << std::endl;
+		}
+	);
+	node.RegisterForLowerFunc(10,
+		[](P2pCore::Pointer core, Session::Pointer session, 
+				const utl::ByteArray& byte_array){ //on receive func from lower
+			std::string str(byte_array.begin(), byte_array.end());
+			std::cout << "onreceive FROM LOWER called:" << str << std::endl;
+			core->BroadcastToUpper(byte_array);
+		}
+	);
+
+	auto core_ptr = node.GetCorePtr();
+	std::cout << "accept port is " << server_port << std::endl;
+	
+	P2pCoreTestCuiApp(service, core_ptr);
+
+    return 0;
+}
+
+#endif
---
+++ Node.h
@@ -0,0 +1,50 @@
+#pragma once
+//Node:20120825
+#include <iostream>
+#include <map>
+#include "P2pCore.h"
+
+namespace nr
+{
+template<class Key>
+class Node{
+public:
+	using KeyPickupper = boost::function<Key (const utl::ByteArray&)>;
+	
+	Node(boost::asio::io_service& service, int port, int buffer_size, KeyPickupper key_pickupper, std::ostream& os)
+		:service(service), key_pickupper(key_pickupper),
+		core_ptr(P2pCore::Create(service, port, buffer_size, 
+			boost::bind(&Node::CallMatchFunc, this, &this->for_upper_func_dict, _1, _2, _3), 
+			boost::bind(&Node::CallMatchFunc, this, &this->for_lower_func_dict, _1, _2, _3),
+			os)){}
+
+	auto RegisterForUpperFunc(const Key& key, P2pCore::OnReceiveFunc func) -> void {
+		this->for_upper_func_dict[key] = func;
+	}
+	
+	auto RegisterForLowerFunc(const Key& key, P2pCore::OnReceiveFunc func) -> void {
+		this->for_lower_func_dict[key] = func;
+	}
+
+	auto GetCorePtr() -> P2pCore::Pointer {
+		return this->core_ptr;	
+	}
+
+private:
+	using FuncDict = std::map<Key, P2pCore::OnReceiveFunc>;
+
+	auto CallMatchFunc(FuncDict* func_dict, P2pCore::Pointer core, 
+			Session::Pointer session, const utl::ByteArray& byte_array) -> void {
+		this->service.post(boost::bind(
+			(*func_dict)[this->key_pickupper(byte_array)], core, session, byte_array));
+	}
+
+	boost::asio::io_service& service;
+	KeyPickupper key_pickupper;
+	FuncDict for_upper_func_dict;
+	FuncDict for_lower_func_dict;
+	P2pCore::Pointer core_ptr;
+
+};
+}
+
