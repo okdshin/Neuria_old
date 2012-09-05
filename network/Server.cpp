b96a788c4382232fa316185fbd4ee0549fb3669c
s.okada <kokuzen@gmail.com>
2012-09-06 02:27:58 +0900

    Acceptor was renamed Server.

---
+++ network/Server.cpp
@@ -0,0 +1,44 @@
+#ifdef SERVER_UNIT_TEST
+#include "Server.h"
+#include "../utility/Utility.h"
+#include <iostream>
+
+using namespace nr;
+using namespace nr::ntw;
+
+int main(int argc, char* argv[])
+{
+	boost::asio::io_service service;
+	boost::asio::io_service::work w(service);
+	int server_port = 54321;
+	if(argc == 2)
+	{
+		server_port = boost::lexical_cast<int>(argv[1]);
+	}
+
+	const int buffer_size = 128;
+	auto session_pool = SessionPool::Create();
+	auto acceptor = Server::Create(service, server_port, buffer_size, 
+		[&session_pool](Session::Pointer session){
+			std::cout << "on_accept_func called:" << std::endl;
+			session_pool->Add(session);
+		},
+		[](Session::Pointer session, const ByteArray& byte_array){ 
+			std::cout << "on receive from accepted session:" 
+				<< utl::ByteArray2String(byte_array) << std::endl;
+		},
+		[&session_pool](Session::Pointer session){
+			session_pool->Erase(session);
+		},
+		std::cout
+	);
+
+	std::cout << "accept port is " << server_port << std::endl;
+	boost::thread t(boost::bind(&boost::asio::io_service::run, &service));
+	
+	t.join();
+
+    return 0;
+}
+
+#endif
---
+++ network/Server.h
@@ -0,0 +1,82 @@
+#pragma once
+//Server:20120905
+#include <iostream>
+#include <vector>
+#include <boost/asio.hpp>
+#include <boost/lexical_cast.hpp>
+#include <boost/thread.hpp>
+#include "Session.h"
+#include "SessionPool.h"
+#include "../ByteArray.h"
+
+namespace nr{
+namespace ntw{
+
+class Server : public boost::enable_shared_from_this<Server> {
+public:
+	using Pointer = boost::shared_ptr<Server>;
+	using OnAcceptFunc = boost::function<void (Session::Pointer)>;
+
+	static auto Create(boost::asio::io_service& service, int port, int buffer_size, 
+			OnAcceptFunc on_accept_func, 
+			Session::OnReceiveFunc on_receive_func,
+			Session::OnCloseFunc on_close_func,
+			std::ostream& os) -> Pointer {
+		auto created = Pointer(
+			new Server(service, port, buffer_size, 
+				on_accept_func, on_receive_func, on_close_func, os));	
+		created->Start();
+		return created;
+	}
+
+private:
+    Server(boost::asio::io_service& service, int port, int buffer_size,
+			OnAcceptFunc on_accept_func, 
+			Session::OnReceiveFunc on_receive_func, 
+			Session::OnCloseFunc on_close_func,
+			std::ostream& os)
+		:service(service), 
+		boost_acceptor(service, boost::asio::ip::tcp::endpoint(
+			boost::asio::ip::tcp::v4(), port)),
+		buffer_size(buffer_size), on_accept_func(on_accept_func), 
+		on_receive_func(on_receive_func), on_close_func(on_close_func), os(os){}
+	
+	auto Start() -> void {
+		auto new_session = Session::Create(this->service, this->buffer_size,
+			this->on_receive_func, this->on_close_func, this->os);
+
+		this->boost_acceptor.async_accept(
+			new_session->GetSocketRef(),
+			boost::bind(
+				&Server::OnAccept, this->shared_from_this(), on_accept_func, new_session,
+				boost::asio::placeholders::error
+			)
+		);
+	}
+
+	auto OnAccept(OnAcceptFunc on_accept_func, Session::Pointer session, 
+			const boost::system::error_code& error_code) -> void {
+		if(!error_code){
+			this->os << "accept:" << GetRemoteAddressStr(session) << ":"
+				<< GetRemotePort(session) << std::endl;
+			this->on_accept_func(session);
+			session->StartReceive();
+		}
+		else{
+			this->os << "accept failure" << std::endl;		
+		}
+		this->Start();
+	}
+
+	boost::asio::io_service& service;
+	boost::asio::ip::tcp::acceptor boost_acceptor;
+	int buffer_size;
+	OnAcceptFunc on_accept_func;
+	Session::OnReceiveFunc on_receive_func;
+	Session::OnCloseFunc on_close_func;
+	std::ostream& os;
+
+};
+
+}
+}
